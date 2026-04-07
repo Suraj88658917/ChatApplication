5ed360d4711d68c96fe66669ad27955e37409ea2
Suraj Saini <suraj88658917@gmail.com>
2026-04-07 10:55:35 +0530

    App.tx to App.js

---
+++ src/screens/service/Socket.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Socket = () => {
+  return (
+    <View>
+      <Text>Socket</Text>
+    </View>
+  )
+}
+
+export default Socket
+
+const styles = StyleSheet.create({})
